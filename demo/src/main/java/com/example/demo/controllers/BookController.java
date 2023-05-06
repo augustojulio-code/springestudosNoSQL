76178b7d9837d3da91fa3baf66fd4aec7b646ad8
Julio <julio.mech@hotmail.com>
2023-05-06 11:52:11 -0300

    Controller class

--- demo/src/main/java/com/example/demo/controllers/BookController.java
+++ demo/src/main/java/com/example/demo/controllers/BookController.java
@@ -1,4 +1,4 @@
-package com.example.controllers;
+package com.example.demo.controllers;
 
 import java.util.List;
 
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.Book;
